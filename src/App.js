4b92a92e94156f0233fe7305bee96df860dae48d
marianoRpi <mmacri@soyhenry.com>
2022-07-28 12:29:21 -0300

    basic app done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Octo from './octo.png'
+import Logo from './logo.png'
+import Fetch from './fetch'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Testing with react-testing-library</h2>
+      <div className='ilustration'>
+        <img src={Logo} alt='react' />
+        <span className='plus'>+</span>
+        <img src={Octo} alt='octopus' />
+      </div>
+      <Fetch url='https://api.chucknorris.io/jokes/random' />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
